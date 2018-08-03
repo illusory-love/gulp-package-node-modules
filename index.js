df519d027e63572a45c0f5a7f7e1827e2719737d
wugang <wugang@yit.com>
2018-08-03 18:52:27 +0800

    兼容插件开发

--- index.js
+++ index.js
@@ -31,6 +31,8 @@ var PLUGIN_NAME = require('./package.json').name;
 var CWD = process.cwd();
 var DIRECTORY = 'npm';
 var MODULEPATH = './node_modules';
+// 主要针对小程序插件开发, 多了一层固定的目录
+var EXCLUDEFOLDER = ['plugin', 'miniprogram'];
 // webpack 的固定配制项
 var WEBPACKCONFIG = {
 	output: {
@@ -40,20 +42,24 @@ var WEBPACKCONFIG = {
 
 	/**
   * 解析打包node_modules模块并替换文件引用 
-  * @param {string} options.dev          开发根目录
-  * @param {string} options.dist         项目打包根目录
-  * @param {string} options.output       打包后的模块存放的目录
+  * @param {string} options.dev           开发根目录
+  * @param {string} options.dist          项目打包根目录
+  * @param {string} options.npmFolder   打包后的模块存放的目录
   * @param {boolean} options.isLiveUpdate 是否实时打包更新node_modules指定模块
   */
 };function PackageNodeModule(_ref) {
 	var dev = _ref.dev,
 	    dist = _ref.dist,
-	    output = _ref.output,
+	    npmFolder = _ref.npmFolder,
 	    isLiveUpdate = _ref.isLiveUpdate;
 
 	// 声明文件复制数组, 防止同一个文件被复制多次
 	// 因下述 webpack 操作实在找不到符合要求的同步实现
 	var copyFiles = [];
+	// 获取当前小程序项目的配置文件
+	var miniConifg = require(_path2.default.join(CWD, dev, 'project.config.json'));
+	// 当前是否是插件开发
+	var isPlugins = miniConifg.compileType === 'plugin';
 
 	return _through2.default.obj(function (file, enc, cb) {
 		if (!dist || !dev) {
@@ -77,31 +83,43 @@ var WEBPACKCONFIG = {
 		// 去除开发目录的绝对路径
 		var fileDeep = fileFullPath.replace(fileBasePath, '');
 		// 计算出当前文件相对开发目录的层级数
-		var deep = fileDeep.split(/\/|\\/).length;
-		// 得到相对路径的返回层级数
-		// /app.js => ./
-		// /utils/utils.js => ../
-		var deepStr = deep == 2 ? './' : '../'.repeat(deep - 2);
+		var deepArray = fileDeep.split(/\/|\\/);
+		var deep = deepArray.length;
+		// 得到相对路径的返回层级数 (插件开发相对小程序开发在指定的开发目录下多了一层固定目录, 因此 -3 ))
+		// /npm/xxx/xx/index.js
+		// /app.js         = > ./
+		// /utils/utils.js = > ../
+		var deepStr = deep == 2 ? './' : '../'.repeat(deep - (isPlugins ? 3 : 2));
+		// 目标输出目录与模块之间可能存在的目录
+		var extraFolder = isPlugins ? deepArray[1] : '';
 
 		// 待替换处理的字符内容
 		var replaceStr = file.contents.toString();
 		// 匹配 module 引用语句
-		var replaceReg = /import.+from.+(['"`])[^\/\.][\w-\/]+\1|require\(\s*(['"`])[^\/\.][\w-\/]+\2/g;
+		var replaceReg = /import.+from?.+(['"`])[^\/\.][\w-\/]+\1|require\(\s*(['"`])[^\/\.][\w-\/]+\2/g;
 
 		// 匹配文本内的npm模块引用
 		var results = replaceStr.replace(replaceReg, function (n) {
 			// 获取当前模块名
-			var moduleName = n.match(/(['"`])[\w-\/]+\1$/)[0].replace(/^['"`]|['"`]$/g, '');
+			var moduleName = n.match(/(['"`])([\w-\/]+)\1$/)[2];
 			// 目标的文件目录
-			var folderPath = _path2.default.resolve(dist, output || DIRECTORY, moduleName);
+			// 用户指定的输出目录(dist) + 额外的目录 (比如插件开发) + npm文件夹名 (npmFolder 或默认 DIRECTORY) + 当前模块名 (moduleName)
+			var folderPath = _path2.default.resolve(dist, extraFolder, npmFolder || DIRECTORY, moduleName);
+			// 模块源目录
 			var modulePath = _path2.default.resolve(MODULEPATH, moduleName);
+			// 模块输出目录是否存在
 			var folderExist = _fsExtra2.default.existsSync(folderPath);
-			var moduleExist = _fsExtra2.default.existsSync(modulePath) || _fsExtra2.default.existsSync(modulePath + '.js');
+			// 模块源目录是否存在 (模块是否安装z)
+			var moduleExist = _fsExtra2.default.existsSync(modulePath);
 			// 获取当前js文件相对于npm模块的引用路径
-			var npmDirctory = deepStr + DIRECTORY + '/' + moduleName + '/index.js';
+			var npmDirctory = '';
 			// 判断是否需要复制模块文件
 			if (moduleExist) {
+				// 记录当前需要替换的模块路径
+				npmDirctory = deepStr + DIRECTORY + '/' + moduleName + '/index.js';
+				// 是否符合复制文件的要求
 				if ((!folderExist || isLiveUpdate) && !copyFiles.includes(moduleName)) {
+					// 记录当前模块
 					copyFiles.push(moduleName);
 					// 当前文件不存在或指定了实时更新才复制文件
 					// 判断当前模块是否已安装
@@ -114,7 +132,6 @@ var WEBPACKCONFIG = {
 					});
 				}
 			} else {
-				npmDirctory = '';
 				console.warn(('\u6A21\u5757 ' + moduleName + ' \u4E0D\u6B63\u786E\u6216\u672A\u5B89\u88C5').yellow);
 			}
 			// 返回替换完成后的模块引用路径
--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ const PLUGIN_NAME = require('./package.json').name
 const CWD         = process.cwd()
 const DIRECTORY   = 'npm'
 const MODULEPATH  = './node_modules'
+// 主要针对小程序插件开发, 多了一层固定的目录
+const EXCLUDEFOLDER = ['plugin', 'miniprogram']
 // webpack 的固定配制项
 const WEBPACKCONFIG = {
 	output: {
@@ -20,15 +22,19 @@ const WEBPACKCONFIG = {
 
 /**
  * 解析打包node_modules模块并替换文件引用 
- * @param {string} options.dev          开发根目录
- * @param {string} options.dist         项目打包根目录
- * @param {string} options.output       打包后的模块存放的目录
+ * @param {string} options.dev           开发根目录
+ * @param {string} options.dist          项目打包根目录
+ * @param {string} options.npmFolder   打包后的模块存放的目录
  * @param {boolean} options.isLiveUpdate 是否实时打包更新node_modules指定模块
  */
-function PackageNodeModule({dev, dist, output, isLiveUpdate}){
+function PackageNodeModule({dev, dist, npmFolder, isLiveUpdate}){
 	// 声明文件复制数组, 防止同一个文件被复制多次
 	// 因下述 webpack 操作实在找不到符合要求的同步实现
-	let copyFiles = []
+	const copyFiles  = []
+	// 获取当前小程序项目的配置文件
+	const miniConifg = require(path.join(CWD, dev, 'project.config.json'))
+	// 当前是否是插件开发
+	const isPlugins  = miniConifg.compileType === 'plugin'
 
 	return through2.obj(function(file, enc, cb){
 		if (!dist || !dev) {
@@ -45,6 +51,7 @@ function PackageNodeModule({dev, dist, output, isLiveUpdate}){
 			return cb()
 		}
 
+
 		// 当前文件的绝对路径
 		const fileFullPath = file.history[0]
 		// 当前文件的开发目录路径, 由 src 里的 base 决定
@@ -52,31 +59,43 @@ function PackageNodeModule({dev, dist, output, isLiveUpdate}){
 		// 去除开发目录的绝对路径
 		const fileDeep     = fileFullPath.replace(fileBasePath, '')
 		// 计算出当前文件相对开发目录的层级数
-		const deep         = fileDeep.split(/\/|\\/).length
-		// 得到相对路径的返回层级数
-		// /app.js => ./
-		// /utils/utils.js => ../
-		const deepStr      = deep == 2 ? './' : '../'.repeat(deep - 2)
+		const deepArray    = fileDeep.split(/\/|\\/)
+		const deep         = deepArray.length
+		// 得到相对路径的返回层级数 (插件开发相对小程序开发在指定的开发目录下多了一层固定目录, 因此 -3 ))
+		// /npm/xxx/xx/index.js
+		// /app.js         = > ./
+		// /utils/utils.js = > ../
+		const deepStr      = deep == 2 ? './' : '../'.repeat(deep - (isPlugins ? 3 : 2))
+		// 目标输出目录与模块之间可能存在的目录
+		const extraFolder  = isPlugins ? deepArray[1] : ''
 
 		// 待替换处理的字符内容
 		const replaceStr = file.contents.toString()
 		// 匹配 module 引用语句
-		const replaceReg = /import.+from.+(['"`])[^\/\.][\w-\/]+\1|require\(\s*(['"`])[^\/\.][\w-\/]+\2/g
+		const replaceReg = /import.+from?.+(['"`])[^\/\.][\w-\/]+\1|require\(\s*(['"`])[^\/\.][\w-\/]+\2/g
 
 		// 匹配文本内的npm模块引用
 		const results = replaceStr.replace(replaceReg,  (n) => {
 				// 获取当前模块名
-			    let moduleName = n.match(/(['"`])[\w-\/]+\1$/)[0].replace(/^['"`]|['"`]$/g, '')
-			    // 目标的文件目录
-				const folderPath  = path.resolve(dist, output || DIRECTORY, moduleName)
+			    const moduleName  = n.match(/(['"`])([\w-\/]+)\1$/)[2]
+				// 目标的文件目录
+				// 用户指定的输出目录(dist) + 额外的目录 (比如插件开发) + npm文件夹名 (npmFolder 或默认 DIRECTORY) + 当前模块名 (moduleName)
+				const folderPath  = path.resolve(dist, extraFolder, npmFolder || DIRECTORY, moduleName)
+				// 模块源目录
 				const modulePath  = path.resolve(MODULEPATH, moduleName)
+				// 模块输出目录是否存在
 				const folderExist = fs.existsSync(folderPath)
-				const moduleExist = fs.existsSync(modulePath) || fs.existsSync(`${modulePath}.js`)
+				// 模块源目录是否存在 (模块是否安装z)
+				const moduleExist = fs.existsSync(modulePath)
 				// 获取当前js文件相对于npm模块的引用路径
-				let   npmDirctory = `${deepStr + DIRECTORY}/${moduleName}/index.js`
+				let   npmDirctory = ''
 				// 判断是否需要复制模块文件
 				if (moduleExist){
+					// 记录当前需要替换的模块路径
+					npmDirctory = `${deepStr + DIRECTORY}/${moduleName}/index.js`;
+					// 是否符合复制文件的要求
 					if ((!folderExist || isLiveUpdate) && !copyFiles.includes(moduleName)){
+						// 记录当前模块
 						copyFiles.push(moduleName)
 						// 当前文件不存在或指定了实时更新才复制文件
 						// 判断当前模块是否已安装
@@ -87,7 +106,6 @@ function PackageNodeModule({dev, dist, output, isLiveUpdate}){
 						webpack(WEBPACKCONFIG, (err, stat) => console.log(`模块 ${moduleName} 复制成功`.cyan))
 					}
 				} else {
-					npmDirctory = ''
 					console.warn(`模块 ${moduleName} 不正确或未安装`.yellow)
 				}
 				// 返回替换完成后的模块引用路径
